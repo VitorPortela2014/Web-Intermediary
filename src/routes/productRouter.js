591fc1db9f4ec19e26c922215a7da63eb35d9b7e
YanBarbosaLouzada <yan.barbosalouzada@gmail.com>
2024-07-22 10:36:54 -0300

    Deploy2

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -5,7 +5,7 @@ import UserController from "../controllers/userController.js";
 
 const productRouter = Router();
 
-productRouter.get("/", UserController.authenticateToken, ProductController.getAllProducts);
+productRouter.get("/", ProductController.getAllProducts);
 // Rotas tipo post servem para receber dados e criar algo
 productRouter.post("/create-product", UserController.authenticateToken, ProductController.createdProduct);
 // Rotas do tipo PUT ou PATCH servem para editar os dados
