7214f87455fa401122a545b18ef02c0b5196a347
YanBarbosaLouzada <yan.barbosalouzada@gmail.com>
2024-07-22 13:44:52 -0300

    Deploy

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,53 +1,39 @@
-import { Product } from '../models/product.js';
-
+import { Product } from "../models/product.js";
 export default class ProductController {
     static async getAllProducts(req, res) {
-        try {
-            const products = await Product.find();
-            return res.json({ products });
-        } catch (error) {
-            return res.status(500).json({ message: 'Error fetching products', error });
-        }
+        const products = await Product.find();
+        return res.json({ products });
     }
-
-    static async createProduct(req, res) {
-        try {
-            const { name, description, quantity } = req.body;
-            const product = new Product({ name, description, quantity });
-            const createdProduct = await product.save();
-            return res.json({ message: 'Criado com sucesso!', data: createdProduct });
-        } catch (error) {
-            return res.status(500).json({ message: 'Error creating product', error });
-        }
+    static async createdProduct(req, res) {
+        const { name, description, quantity } = req.body;
+        const data = new Product();
+        data.name = name;
+        data.description = description;
+        data.quantity = quantity;
+        const createdProduct = await Product.create(data);
+        return res.json({ message: "Criado com sucesso!", data: createdProduct });
     }
-
     static async editProduct(req, res) {
-        try {
-            const { _id, name, quantity, description } = req.body;
-            const product = await Product.findById(_id);
-            if (!product) {
-                return res.status(404).json({ message: 'Não existe um produto com este id!' });
-            }
-            product.name = name;
-            product.quantity = quantity;
-            product.description = description;
-            const updatedProduct = await product.save();
-            return res.json({ message: 'Editado com sucesso!', data: updatedProduct });
-        } catch (error) {
-            return res.status(500).json({ message: 'Error editing product', error });
+        const { _id, name, quantity, description } = req.body;
+        const product = await Product.findById(_id);
+        if (!product) {
+            return res.json({ message: "Não existe um produto com este id!" });
         }
+        const updateFields = {};
+        if (name) updateFields.name = name;
+        if (quantity) updateFields.quantity = quantity;
+        if (description) updateFields.description = description;
+        // Perform the update operation
+        const updatedProduct = await Product.findByIdAndUpdate(
+            _id,
+            { $set: updateFields },
+            { new: true }
+        );
+        return res.json({ message: "Editado com sucesso!", updatedProduct });
     }
-
     static async deleteProduct(req, res) {
-        try {
-            const { id } = req.params;
-            const deletedProduct = await Product.findByIdAndDelete(id);
-            if (!deletedProduct) {
-                return res.status(404).json({ message: 'Não existe um produto com este id!' });
-            }
-            return res.json({ message: 'Deletado com sucesso!' });
-        } catch (error) {
-            return res.status(500).json({ message: 'Error deleting product', error });
-        }
+        const { id } = req.params;
+        await Product.findOneAndDelete(id);
+        return res.json({ message: "Deletado com sucesso!" });
     }
 }
--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -7,10 +7,21 @@ const productRouter = Router();
 
 productRouter.get("/", ProductController.getAllProducts);
 // Rotas tipo post servem para receber dados e criar algo
-productRouter.post("/create-product", UserController.authenticateToken, ProductController.createProduct);
+productRouter.post(
+    "/create-product",
+    UserController.authenticateToken,
+    ProductController.createdProduct
+);
 // Rotas do tipo PUT ou PATCH servem para editar os dados
-productRouter.put("/edit-product", UserController.authenticateToken, ProductController.editProduct);
+productRouter.put(
+    "/edit-product",
+    UserController.authenticateToken,
+    ProductController.editProduct
+);
 // Rotas do tipo delete servem para deletar o código
-productRouter.delete("/delete-product/:id", UserController.authenticateToken, ProductController.deleteProduct);
-
+productRouter.delete(
+    "/delete-product/:id",
+    UserController.authenticateToken,
+    ProductController.deleteProduct
+);
 export default productRouter;
